4294dc92ff99a9d4bcbee0a587cf01510cd23f4f
kozy810 <koji.19960810@gmail.com>
2022-06-08 00:18:50 +0900

    first commit

---
+++ networks.ts
@@ -0,0 +1,18 @@
+import { NetworksUserConfig } from "hardhat/types";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const networks: NetworksUserConfig = {};
+
+if (process.env.PRIVATE_KEY) {
+  networks.rinkeby = {
+    chainId: 4,
+    url: process.env.RINKEBY_RPC,
+    accounts: [process.env.PRIVATE_KEY]
+  };
+} else {
+  networks.hardhat = {}
+}
+
+export default networks;
