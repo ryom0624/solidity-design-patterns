d5ea1f2f8a870252482abb1ea6587e8bd536454c
ryo murakami <ryo.m0624@gmail.com>
2022-08-30 20:25:37 +0900

    update

--- networks.ts
+++ networks.ts
@@ -1,19 +1,19 @@
 import { NetworksUserConfig } from "hardhat/types";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const networks: NetworksUserConfig = {};
 
 if (process.env.PRIVATE_KEY) {
-  networks.rinkeby = {
-    chainId: 4,
-    url: process.env.RINKEBY_RPC,
-    accounts: [process.env.PRIVATE_KEY],
-  };
+  // networks.rinkeby = {
+  //   chainId: 4,
+  //   url: process.env.RINKEBY_RPC,
+  //   accounts: [process.env.PRIVATE_KEY],
+  // };
   networks.goerli = {
     chainId: 5,
-    url: process.env.RINKEBY_RPC,
+    url: process.env.GOERLI_URL,
     accounts: [process.env.PRIVATE_KEY],
   };
 } else {
